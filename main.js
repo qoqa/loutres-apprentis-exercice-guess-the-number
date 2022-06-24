1161cb7cd00d5f214a1a9711274d01051efe940b
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2022-06-24 14:04:18 +0200

    fix game

--- main.js
+++ main.js
@@ -30,22 +30,24 @@ function handleGame() {
 }
 
 function stopGame() {
-  handleField(false);
   numberToFind = undefined;
+
+  handleField(true);
 }
 
 function startGame() {
   numberToFind = Math.floor(Math.random() * 100) + 1;
   triesTimes = 0;
 
-  handleField(true);
+  handleField(false);
 }
 
 function handleField(start) {
+  console.log(start);
   gameButton.innerText = start ? 'Start' : 'Stop';
-  guessForm[0].disabled = !start;
-  guessForm[1].disabled = !start;
-  formWrapper.children['guess-form-info'].style.display = start ? 'none' : 'block';
+  guessForm[0].disabled = start;
+  guessForm[1].disabled = start;
+  formWrapper.children['guess-form-info'].style.display = start ? 'block' : 'none';
 }
 
 // on submit button clicked
@@ -71,6 +73,10 @@ function testProposal(guessedNumber) {
 
   if (guessedNumber === numberToFind) {
     state = guessNumberState.CORRECT;
+  } else if (guessedNumber > numberToFind) {
+    state = guessNumberState.TO_HIGH;
+  } else {
+    state = guessNumberState.TO_LOW;
   }
 
   renderResult(guessedNumber, state, triesTimes);
